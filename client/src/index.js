3855eb67bbb1686626053db69919255a6e32eef5
taeyun <lwe12f675@gmail.com>
2025-04-24 05:00:15 +0900

    client: server: 트윗 APIs를 사용하는 프론트엔드 구현 (서버에서 데이터 받아와 렌더링)

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import AuthService from './service/auth';
-import TweetService from './service/tweet';
-import { BrowserRouter } from 'react-router-dom';
-import { AuthProvider } from './context/AuthContext';
-import { AuthErrorEventBus } from './context/AuthContext';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import AuthService from "./service/auth";
+import TweetService from "./service/tweet";
+import { BrowserRouter } from "react-router-dom";
+import { AuthProvider } from "./context/AuthContext";
+import { AuthErrorEventBus } from "./context/AuthContext";
 
 const baseURL = process.env.REACT_APP_BASE_URL;
 const authErrorEventBus = new AuthErrorEventBus();
@@ -24,5 +24,5 @@ ReactDOM.render(
       </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
--- client/src/service/tweet.js
+++ client/src/service/tweet.js
@@ -1,43 +1,91 @@
 export default class TweetService {
-  tweets = [
-    {
-      id: 1,
-      text: "드림코딩에서 강의 들으면 너무 좋으다",
-      createdAt: "2021-05-09T04:20:57.000Z",
-      name: "Bob",
-      username: "bob",
-      url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSyPkxMuo6NOHcNx-aO-wOo3eyVnB2oTq-ZwA&s",
-    },
-  ];
+  constructor(baseURL) {
+    // this.baseURL = "http://localhost:8080";
+    this.baseURL = baseURL;
+  }
+
+  // 더미데이터
+  // tweets = [
+  //   {
+  //     id: 1,
+  //     text: "더미더미 데이터 테스트",
+  //     createdAt: "2021-05-09T04:20:57.000Z",
+  //     name: "Bob",
+  //     username: "bob",
+  //     url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSyPkxMuo6NOHcNx-aO-wOo3eyVnB2oTq-ZwA&s",
+  //   },
+  // ];
 
   async getTweets(username) {
-    return username
-      ? this.tweets.filter((tweet) => tweet.username === username)
-      : this.tweets;
+    // username이 있으면 해당 username의 트윗만 가져오고, 없으면 모든 트윗을 가져오도록 조건 추가
+    const query = username ? `?username=${username}` : "";
+
+    // 서버에서 데이터를 가져오는 요청
+    const response = await fetch(`${this.baseURL}/tweets${query}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    // 받아온 데이터 json으로 변환
+    const data = await response.json();
+
+    // 서버에서 데이터를 가져오는 요청이 실패하면, 즉, 200이 아니라면 에러 던지기
+    if (response.status !== 200) {
+      throw new Error(data.message);
+    }
+    return data;
   }
 
   async postTweet(text) {
-    const tweet = {
-      id: Date.now(),
-      createdAt: new Date(),
-      name: "Ellie",
-      username: "ellie",
-      text,
-    };
-    this.tweets.push(tweet);
-    return tweet;
+    const response = await fetch(`${this.baseURL}/tweets`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ text, name: "Taeyun", username: "taeyun" }),
+    });
+
+    const data = await response.json();
+
+    if (response.status !== 201) {
+      throw new Error(data.message);
+    }
+    return data;
   }
 
   async deleteTweet(tweetId) {
-    this.tweets = this.tweets.filter((tweet) => tweet.id !== tweetId);
+    const response = await fetch(`${this.baseURL}/tweets/${tweetId}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    // DELETE는 따로 데이터를 받아오지 않음
+
+    // 삭제를 실패해서, 상태코드가 204가 아니라면 에러 던지기
+    if (response.status !== 204) {
+      throw new Error("트윗 삭제에 실패했습니다");
+    }
   }
 
   async updateTweet(tweetId, text) {
-    const tweet = this.tweets.find((tweet) => tweet.id === tweetId);
-    if (!tweet) {
-      throw new Error("tweet not found!");
+    const response = await fetch(`${this.baseURL}/tweets/${tweetId}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ text }),
+    });
+
+    const data = await response.json();
+
+    // 수정을 실패해서, 상태코드가 200이 아니라면 에러 던지기
+    if (response.status !== 200) {
+      throw new Error(data.message);
     }
-    tweet.text = text;
-    return tweet;
+    return data; // 수정된 트윗을 반환하여 적용
   }
 }
