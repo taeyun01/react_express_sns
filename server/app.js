2a1cb7673de9590ab6eee3da86b28edb278698a3
taeyun <lwe12f675@gmail.com>
2025-05-09 05:48:03 +0900

    server: mysql 워크벤치에서 스키마 정의 후 노드 서버에 연결해보기

--- server/app.js
+++ server/app.js
@@ -6,6 +6,7 @@ import tweetsRouter from "./router/tweets.js";
 import authRouter from "./router/auth.js";
 import { config } from "./config.js";
 import { initSocket } from "./connection/socket.js";
+import { db } from "./db/database.js";
 
 const app = express();
 
@@ -26,6 +27,8 @@ app.use((error, req, res, next) => {
   console.error(error);
   res.sendStatus(500);
 });
+
+db.getConnection().then((connection) => console.log(connection));
 
 const server = app.listen(config.host.port, () => {
   console.log(`Server is running on port ${config.host.port}`);
